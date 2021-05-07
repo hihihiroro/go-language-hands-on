6804e5c3295a3477227f1145d227612aa38d2a7a
hihihiroro <hihihiroro@gmail.com>
2021-05-07 19:58:14 +0900

    p77

--- ch1/hello/input.go
+++ ch1/hello/input.go
@@ -7,9 +7,8 @@ import (
 )
 
 func Input(msg string) string {
-	scanner := bifop.NewScanner(os.Stdin)
-	fmt.Print(msg + ": ")
 	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print(msg + ": ")
 	scanner.Scan()
 	return scanner.Text()
 }
